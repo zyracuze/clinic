78a53240ad3eb4a0dda4625b48044d1a9c750c6c
Ladarat <ladarat.y@gmail.com>
2017-06-19 20:28:03 +0700

    Demo Router in App Component

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import LoginFrom from './LoginFrom'
+import Home from './Home'
 import Header from './Header'
 import './Login.css'
+import {Router, Route, hashHistory} from 'react-router'
 
 class App extends React.Component {
   render() {
@@ -12,7 +14,10 @@ class App extends React.Component {
           <Header />
         </div>
         <div className="App-intro">
-          <LoginFrom />
+        <Router history={hashHistory}>
+          <Route path='/' component={LoginFrom} />
+          <Route path='/home' component={Home} />
+        </Router>
         </div>
       </div>
     );
