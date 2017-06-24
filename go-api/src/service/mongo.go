eea7159fbcfe4d68500784c9eaff8d09875a9ed3
phatcharaphan <phat.pan@gmail.com>
2017-06-25 01:31:38 +0700

    refactor connect mongo service

--- go-api/src/service/mongo.go
+++ go-api/src/service/mongo.go
@@ -5,67 +5,36 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
-var connection *Connection
-var database *Database
-
 type Connection struct {
-	*mgo.Session
-	db string
+	MogoSession  *mgo.Session
+	DatabaseName string
 }
 
-type Database struct {
-	Interface
-}
-
-func connect() (*Connection, error) {
+func Connect() (*Connection, error) {
 	conf := config.ConfReader
-	if connection != nil {
-		return connection, nil
-	}
 
 	session, err := mgo.Dial(conf.GetString("mongodb.host"))
 	if err != nil {
 		return nil, err
 	}
-
-	session.SetMode(mgo.Strong, true)
-	connection = &Connection{Session: session, db: conf.GetString("mongodb.dbname")}
-	return connection, nil
-}
-
-func Connect() (Interface, error) {
-	if database != nil {
-		return database, nil
-	}
-	conn, err := connect()
-	if err != nil {
-		return nil, err
-	}
-	database = &Database{Interface: conn}
-	return database, nil
-}
-func (conn *Connection) copySession() *mgo.Session {
-	return conn.Copy()
-}
-
-func Close() {
-	if connection != nil {
-		connection.Close()
-	}
+	session.SetMode(mgo.Monotonic, true)
+	db := conf.GetString("mongodb.dbname")
+	return &Connection{
+		MogoSession:  session,
+		DatabaseName: db,
+	}, nil
 }
 
 func (conn *Connection) FindOne(collection string, query interface{}, data interface{}) error {
-	session := conn.copySession()
-	defer session.Close()
-	c := session.DB(conn.db).C(collection)
+	session := conn.MogoSession.Clone()
+	defer session.Clone()
+	c := session.DB(conn.DatabaseName).C(collection)
 	return c.Find(query).One(data)
 }
 
-func (ld *Database) FindOne(collection string, query interface{}, data interface{}) error {
-	err := ld.Interface.FindOne(collection, query, data)
-	return err
-}
-
-type Interface interface {
-	FindOne(collection string, query interface{}, data interface{}) error
+func (conn *Connection) Insert(collection string, data interface{}) error {
+	session := conn.MogoSession.Clone()
+	defer session.Clone()
+	c := session.DB(conn.DatabaseName).C(collection)
+	return c.Insert(&data)
 }
