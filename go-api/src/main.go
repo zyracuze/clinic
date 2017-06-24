dda5e670316694e286d5ea1f832a1fa2fea320fd
phatcharaphan <phat.pan@gmail.com>
2017-06-25 01:30:45 +0700

    refactor login

--- go-api/src/main.go
+++ go-api/src/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"net/http"
+	login "./login"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
-
-type User struct {
-	Username string `json:"userName"`
-	Password string `json:"password"`
-}
 
 func main() {
 	e := echo.New()
@@ -20,19 +15,6 @@ func main() {
 		AllowMethods: []string{echo.POST},
 	}))
 
-	e.POST("/login", ValidateUserLogin)
+	e.POST("/login", login.ValidateUserLogin)
 	e.Logger.Fatal(e.Start(":8888"))
-}
-
-func ValidateUserLogin(c echo.Context) error {
-	user := new(User)
-	err := c.Bind(user)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, nil)
-	}
-	if user.Username == "phatpan" && user.Password == "1234" {
-		return c.String(http.StatusOK, "login success")
-	}
-
-	return c.String(http.StatusUnauthorized, "Not found.")
 }
