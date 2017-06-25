15583693a46f84564c0120c75c22b93b481533d7
phatcharaphan <phat.pan@gmail.com>
2017-06-25 13:44:07 +0700

    add new method update

--- go-api/src/service/mongo.go
+++ go-api/src/service/mongo.go
@@ -38,3 +38,11 @@ func (conn *Connection) Insert(collection string, data interface{}) error {
 	c := session.DB(conn.DatabaseName).C(collection)
 	return c.Insert(&data)
 }
+
+func (conn *Connection) Update(collection string, query interface{}, data interface{}) error {
+	session := conn.MogoSession.Clone()
+	defer session.Clone()
+	c := session.DB(conn.DatabaseName).C(collection)
+	err := c.Update(query, data)
+	return err
+}
