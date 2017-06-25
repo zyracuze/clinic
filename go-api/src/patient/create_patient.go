8663409a1c523802909c671f18d1877e44bd7052
phatcharaphan <phat.pan@gmail.com>
2017-06-25 12:36:45 +0700

    insert patient in mongo

--- go-api/src/patient/create_patient.go
+++ go-api/src/patient/create_patient.go
@@ -1,28 +1,60 @@
 package patient
 
+import (
+	"net/http"
+
+	database "../service"
+	"github.com/labstack/echo"
+)
+
 type Patient struct {
-	FirstName         string `json:"firstname"`
-	Lastname          string `json:"lastname"`
-	Nickname          string `json:"nickname"`
-	Gender            string `json:"gender"`
-	Birthday          string `json:"birthday"`
-	IdCard            string `json:"idCard"`
-	Career            string `json:"career"`
-	Tel               string `json:"tel"`
-	WorkAddress       string `json:"workAddress"`
-	HomeAddress       string `json:"homeAddress"`
-	RequiredDocument  string `json:"requiredDocument"`
-	CongenitalDisease string `json:"congenitalDisease"`
-	BeAllergic        string `json:"beAllergic"`
-	EmergencyContact  Emergency
+	FirstName         string    `json:"firstname"`
+	Lastname          string    `json:"lastname"`
+	Nickname          string    `json:"nickname"`
+	Gender            string    `json:"gender"`
+	Birthday          string    `json:"birthday"`
+	IdCard            string    `json:"idCard"`
+	Career            string    `json:"career"`
+	Tel               string    `json:"tel"`
+	WorkAddress       string    `json:"workAddress"`
+	HomeAddress       string    `json:"homeAddress"`
+	RequiredDocument  string    `json:"requiredDocument"`
+	CongenitalDisease string    `json:"congenitalDisease"`
+	BeAllergic        string    `json:"beAllergic"`
+	EmergencyContact  Emergency `json:"emergencyContact"`
 }
 
 type Emergency struct {
 	Name         string `json:"name"`
 	Relationship string `json:"relationship"`
-	Tel          string `json:"Tel"`
+	Tel          string `json:"tel"`
 }
 
-func CreatePatient() {
+func CreatePatient(c echo.Context) error {
+	p := new(Patient)
 
+	err := c.Bind(p)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	err = p.insertPatient()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	return c.JSON(http.StatusCreated, "Create patient success.")
+}
+
+func (p *Patient) insertPatient() error {
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+
+	err = db.Insert("patient", p)
+	if err != nil {
+		return err
+	}
+	return nil
 }
