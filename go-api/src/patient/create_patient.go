175c4e40c8fa0b61001a9ac6c1801468b8918848
phatcharaphan <phat.pan@gmail.com>
2017-06-25 14:36:05 +0700

    add generate id patient

--- go-api/src/patient/create_patient.go
+++ go-api/src/patient/create_patient.go
@@ -1,7 +1,11 @@
 package patient
 
 import (
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	"time"
 
 	database "../service"
 	"github.com/labstack/echo"
@@ -10,6 +14,7 @@ import (
 
 type Patient struct {
 	Id                bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	IdPatient         string        `json:"idPatient"`
 	FirstName         string        `json:"firstname"`
 	Lastname          string        `json:"lastname"`
 	Nickname          string        `json:"nickname"`
@@ -39,12 +44,37 @@ func CreatePatient(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
+	patient.IdPatient = generateIdPatient()
 	err = patient.insertPatient()
 	if err != nil {
 		return c.NoContent(http.StatusConflict)
 	}
 
 	return c.NoContent(http.StatusCreated)
+}
+
+func generateIdPatient() string {
+	t := time.Now().Local()
+	dateTime := t.Format("20060102150405")
+	count, err := count()
+	if err != nil {
+		count = rand.Intn(10)
+	}
+	result := "P" + dateTime + strconv.Itoa(count+1)
+	return result
+}
+
+func count() (int, error) {
+	db, err := database.Connect()
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := db.CountCollection("patient")
+	if err != nil {
+		return 0, err
+	}
+	return result, nil
 }
 
 func (p *Patient) insertPatient() error {
