5dbbee7708f32e2c5407a03ae6a0eb04f810a3fd
phatcharaphan <phat.pan@gmail.com>
2017-06-25 13:43:49 +0700

    update patient in mongo

---
+++ go-api/src/patient/update_patient.go
@@ -0,0 +1,42 @@
+package patient
+
+import (
+	"net/http"
+
+	database "../service"
+	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func UpdatePatient(c echo.Context) error {
+	patient := new(Patient)
+	id := c.Param("id")
+	patient.Id = bson.ObjectIdHex(id)
+	if err := c.Bind(patient); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	err := patient.updatePatient(patient)
+	if err != nil {
+		return c.NoContent(http.StatusConflict)
+	}
+	return c.NoContent(http.StatusOK)
+}
+
+func (p *Patient) updatePatient(patient *Patient) error {
+	db, err := database.Connect()
+	if err != nil {
+		return err
+	}
+
+	change := bson.M{"$set": &p}
+	find := bson.M{
+		"_id": p.Id,
+	}
+
+	err = db.Update("patient", find, change)
+	if err != nil {
+		return err
+	}
+	return nil
+}
