7a26c48ac3ad43ea303645ed8d16641ebdcd6aca
somkiat <somkiat.p@gmail.com>
2017-06-27 18:30:22 +0700

    Update build script with parameter refresh

--- go-api/build.sh
+++ go-api/build.sh
@@ -1,14 +1,32 @@
-# rm -rf pkg
-# rm -rf bin
-
+#!/bin/bash
 export GOPATH=`pwd`
 
-gofmt -w src
+# gofmt -w src
 
-go get github.com/spf13/viper
-go get gopkg.in/mgo.v2
-go get gopkg.in/yaml.v2
+if [ "$1" = "refresh" ]
+then
+  rm -rf pkg
+  rm -rf bin
+  rm -rf $GOPATH/src/gopkg.in/
+  rm -rf $GOPATH/src/golang.org/
+  rm -rf $GOPATH/src/github.com/
 
+  echo "Downloading dependencies ...."
+  go get -u github.com/labstack/echo/...
+  go get -u github.com/spf13/viper
+
+  # go get gopkg.in/mgo.v2
+  # go get gopkg.in/yaml.v2
+  git clone -b v2 git@github.com:go-mgo/mgo.git $GOPATH/src/gopkg.in/mgo.v2
+  git clone -b v2 git@github.com:go-yaml/yaml.git $GOPATH/src/gopkg.in/yaml.v2
+
+fi
+
+echo "Testing ...."
+go test patient/...
+
+echo "Building ...."
 go install main
 
-./bin/main
+echo "Finished"
+# ./bin/main
