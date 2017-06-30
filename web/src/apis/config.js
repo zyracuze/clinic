6111cdc62c55e7805d28f9b817fe58164c4b1ae7
phatcharaphan <phat.pan@gmail.com>
2017-06-30 16:28:51 +0700

    add config in react

---
+++ web/src/apis/config.js
@@ -0,0 +1,6 @@
+const URL = "http://localhost:8888/"; 
+export default{
+    LOGIN_URL : URL + "login",
+    CREATE_PATIENT_URL : URL + "patient/create",
+    SEARCH_PATIENT_URL : URL + "patient/search"
+}
