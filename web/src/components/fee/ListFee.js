76b4c966ca8141f622bbe374a7e4592f1ba33b9a
phatcharaphan <phat.pan@gmail.com>
2017-07-15 18:11:57 +0700

    #11 refactor new list fee

--- web/src/components/fee/ListFee.js
+++ web/src/components/fee/ListFee.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import {FormGroup, FormControl, ControlLabel, Col } from 'react-bootstrap';
+import {FormGroup, FormControl, ControlLabel, Col, Button } from 'react-bootstrap';
 export default class FormGroupTwoBlock extends Component {
-    render() {
-        return (
-          <FormGroup>
+    state = {
+        feeblock:[],
+        formGroup: [(
+             <FormGroup>
               <Col componentClass={ControlLabel} sm={3}>รายการค่าใช้จ่าย</Col>
               <Col sm={3}>
                   <FormControl id="expenseItem0" type="text" placeholder="รายการค่าใช้จ่าย" />
@@ -13,6 +14,18 @@ export default class FormGroupTwoBlock extends Component {
                   <FormControl id="amount0" type="text" placeholder="จำนวนเงิน" />
               </Col>
           </FormGroup>
+        )]
+    }
+    onAddBtnClick=()=>{
+        this.setState({feeblock: this.state.feeblock.concat((this.state.formGroup))});
+    }
+    render() {
+        return (
+          <div>
+            {this.state.formGroup}
+            {this.state.feeblock}
+            <Button onClick={this.onAddBtnClick} bsStyle="btn btn-primary width45" type="submit" id="saveBtn" >+</Button>
+          </div>
         );
     }
 }
--- web/src/components/fee/SaveFee.js
+++ web/src/components/fee/SaveFee.js
@@ -4,8 +4,7 @@ import { Form, FormGroup, FormControl, ControlLabel, Col, Button, Glyphicon, Inp
 import ListFee from './ListFee'
 import DisplayPatientName from '../elements/DisplayPatientName'
 
-class SaveFee extends Component {
-
+export default class SaveFee extends Component {
   constructor() {
     super();
     this.state = {
@@ -16,16 +15,10 @@ class SaveFee extends Component {
       lastname: 'bug',
       lastnamePatient: "",
       firstnamePatient: "",
-      feeblock: [(
-        <ListFee />
-      )]
     }
-
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
-    this.onAddBtnClick = this.onAddBtnClick.bind(this);
     this.handleSearchPatient = this.handleSearchPatient.bind(this);
-
   }
 
   handleCloseModal() {
@@ -58,19 +51,8 @@ class SaveFee extends Component {
     event.preventDefault();
   }
 
-  onAddBtnClick(event) {
-    const feeblock = this.state.feeblock;
-    this.setState({
-      feeblock: feeblock.concat((
-        <ListFee />
-      ))
-    });
-  }
-
   render() {
-
     return (
-
       <Form horizontal onSubmit={this.handleSubmit}>
         <FormGroup>
           <Col componentClass={ControlLabel} sm={3}>
@@ -87,20 +69,14 @@ class SaveFee extends Component {
         </FormGroup>
         <DisplayPatientName firstnamePatient={this.state.firstnamePatient}
                             lastnamePatient={this.state.lastnamePatient}/>
-        {this.state.feeblock}
-        <div>
-          <Button onClick={this.onAddBtnClick} bsStyle="btn btn-primary width45" type="submit" id="saveBtn" >+</Button>
-        </div>
+        <ListFee/>
         <Button bsStyle="success" type="submit" id="saveBtn" >เพิ่ม</Button>
-
         <ModalSearch handleCloseModal={this.handleCloseModal} 
                      isShowingModal={this.state.isShowingModal}
                      dataObj={this.state.dataObj}
                      onChooseData={this.onChooseData}
                      onSubmitSearchButton={this.onSubmitSearchButton}/>
       </Form>
-      
     )
   }
 }
-export default SaveFee;
