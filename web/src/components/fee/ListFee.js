2ec34705302efad15718f613d344fc3dc1455edb
phatcharaphan <phat.pan@gmail.com>
2017-07-15 17:43:06 +0700

    #11 add list fee for fee

---
+++ web/src/components/fee/ListFee.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {FormGroup, FormControl, ControlLabel, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+export default class FormGroupTwoBlock extends Component {
+    render() {
+        return (
+          <FormGroup>
+              <Col componentClass={ControlLabel} sm={3}>รายการค่าใช้จ่าย</Col>
+              <Col sm={3}>
+                  <FormControl id="expenseItem0" type="text" placeholder="รายการค่าใช้จ่าย" />
+                </Col>
+              <Col componentClass={ControlLabel} sm={1}>จำนวนเงิน</Col>
+              <Col sm={3}>
+                  <FormControl id="amount0" type="text" placeholder="จำนวนเงิน" />
+              </Col>
+          </FormGroup>
+        );
+    }
+}
