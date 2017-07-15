4d0eb86a85a7ed8253e44f21c235e82d08087f55
phatcharaphan <phat.pan@gmail.com>
2017-07-15 19:07:56 +0700

    #11 refactor save fee

--- web/src/containers/fee/SaveFee.js
+++ web/src/containers/fee/SaveFee.js
@@ -1,8 +1,28 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Form, Button} from 'react-bootstrap';
+import ModalSearch from '../../components/fee/ModalSearch'
+import ListFee from '../../components/fee/ListFee'
+import DisplayPatientName from '../../components/elements/DisplayPatientName'
+import SearchForm from '../../components/elements/SearchForm'
 export default class SaveFeeContainer extends Component{
+    state = {
+        firstnamePatient: "",
+        lastnamePatient: ""
+    }
     render(){
         return(
-            
+        <Form horizontal onSubmit={this.onSubmitSaveFee}>
+            <SearchForm onSearchPatient={this.onSearchPatient}/>
+            <DisplayPatientName firstnamePatient={this.state.firstnamePatient}
+                                lastnamePatient={this.state.lastnamePatient}/>
+            <ListFee/>
+            <Button bsStyle="success" type="submit" id="saveBtn" >เพิ่ม</Button>
+            <ModalSearch onCloseModal={this.onCloseModal} 
+                        isShowingModal={this.state.isShowingModal}
+                        dataObjTable={this.state.dataObjTable}
+                        onChooseDataTable={this.onChooseDataTable}
+                        onAdvanceSearchPatient={this.onAdvanceSearchPatient}/>
+        </Form>
         )
     }
 }
