54f8cbc44ccf8ae2b1a3fcf6edd8620b1f73048c
phatcharaphan <phat.pan@gmail.com>
2017-07-15 19:47:35 +0700

    fixed bug when get value in form submit

--- web/src/components/elements/AdvanceSearchForm.js
+++ web/src/components/elements/AdvanceSearchForm.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react'
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types'
 import '../../App.css';
 export default class AdvanceSearchFormElement extends Component{
+    static propTypes = {
+        onAdvanceSearchPatient: PropTypes.func.isRequired,
+        titleSearch: PropTypes.string.isRequired,
+        namePatient: PropTypes.string.isRequired,
+        lastnamePatient: PropTypes.string.isRequired,
+        idPatient: PropTypes.string.isRequired
+    }
     render(){
         return (
         <form onSubmit = { this.props.onAdvanceSearchPatient }>
@@ -11,17 +19,20 @@ export default class AdvanceSearchFormElement extends Component{
               type="text"
               className="form-control-signin"
               placeholder="ชื่อ"
-              name={this.props.name}/>
+              value={this.props.namePatient}
+              name="namePatient"/>
             <input
               type="text"
               className="form-control-signin"
               placeholder="นามสกุล"
-              name={this.props.lastname}/>
+              value={this.props.lastnamePatient}
+              name="lastnamePatient"/>
           <input
               type="text"
               className="form-control-signin"
               placeholder="รหัสผู้ป่วย"
-              name={this.props.idPatient}/>
+              value={this.props.idPatient}
+              name="idPatient"/>
         </label>
         <Button
             bsStyle="primary"
