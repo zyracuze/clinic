9184938193c0e914976fb739741447c8cb81e3a3
phatcharaphan <phat.pan@gmail.com>
2017-07-15 17:01:11 +0700

    add form search and result in modal

--- web/src/components/fee/ModalSearch.js
+++ web/src/components/fee/ModalSearch.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types';
 import {ModalContainer, ModalDialog} from 'react-modal-dialog';
+import SearchForm from '../elements/SearchForm'
+import Table from '../elements/SharedTable'
+
 export default class ModalSearch extends Component{
     static propTypes = {
         isShowingModal: PropTypes.bool.isRequired,
-        handleCloseModal: PropTypes.func.isRequired
+        handleCloseModal: PropTypes.func.isRequired,
+        dataObj:  PropTypes.array.isRequired,
+        onChooseData: PropTypes.func.isRequired,
+        titleSearch: PropTypes.string.isRequired,
+        onSubmitSearchButton : PropTypes.func.isRequired
     }
     render(){
         return (
@@ -14,7 +21,9 @@ export default class ModalSearch extends Component{
                 <ModalContainer onClose={this.props.handleCloseModal}>
                 <ModalDialog onClose={this.props.handleCloseModal}>
                     <h1>Search Patient</h1>
-                    <p> Comming soon</p>
+                    <SearchForm titleSearch={this.props.titleSearch} 
+                                onSubmitSearchButton={this.props.onSubmitSearchButton}/>
+                    <Table dataObj={this.props.dataObj} onChooseData={this.props.onChooseData}/>
                 </ModalDialog>
                 </ModalContainer>
             }
