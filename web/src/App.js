0c9371a89d5e5fe10d440bf2f38dc355d092d6bb
Ladarat <ladarat.y@gmail.com>
2017-07-17 22:35:09 +0700

    Change router editComponent and create

--- web/src/App.js
+++ web/src/App.js
@@ -6,8 +6,8 @@ import Header from './components/Header'
 import EditPatientContainer from './containers/EditPatientContainer'
 import GenerateMedicalCertificate from './components/GenerateMedicalCertificate'
 import MedicalExpenseReport from './components/MedicalExpenseReport'
+import CreatePatient from './components/CreatePatient'
 import SaveFee from './containers/fee/SaveFee'
-import EditPatient from './components/EditPatient'
 import Schedule from './components/Schedule'
 import Logout from './components/Logout'
 
@@ -31,9 +31,10 @@ class App extends Component {
           <Route path='/generateMedicalCertificate' component={GenerateMedicalCertificate}/>
           <Route path='/medicalExpenseReport' component={MedicalExpenseReport}/>
           <Route path='/saveFee' component={SaveFee}/>
-          <Route path='/editPatient' component={EditPatient}/>
+          <Route path='/createPatient' component={CreatePatient}/>
           <Route path='/schedule' component={Schedule}/>
           <Route path='/logout' component={Logout}/>
+          
         </Router>
         </div>
       </div>
