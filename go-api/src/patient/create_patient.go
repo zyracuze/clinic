c17195b33e64553746e5842e31b6fc4c7fe51b5d
phatcharaphan <phat.pan@gmail.com>
2017-07-18 21:37:21 +0700

    remove code to modal patient

--- go-api/src/patient/create_patient.go
+++ go-api/src/patient/create_patient.go
@@ -1,49 +1,20 @@
 package patient
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http"
-
+	database "service"
 	"time"
-	"fmt"
 
 	"github.com/labstack/echo"
-	"gopkg.in/mgo.v2/bson"
-	database "service"
 )
-
-type Patient struct {
-	Id                bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
-	IdPatient         string        `json:"idPatient"`
-	FirstName         string        `json:"firstname"`
-	Lastname          string        `json:"lastname"`
-	Nickname          string        `json:"nickname"`
-	Gender            string        `json:"gender"`
-	Birthday          string        `json:"birthday"`
-	IdCard            string        `json:"idCard"`
-	Career            string        `json:"career"`
-	Tel               string        `json:"tel"`
-	WorkAddress       string        `json:"workAddress"`
-	HomeAddress       string        `json:"homeAddress"`
-	RequiredDocument  string        `json:"requiredDocument"`
-	CongenitalDisease string        `json:"congenitalDisease"`
-	BeAllergic        string        `json:"beAllergic"`
-	EmergencyContact  Emergency     `json:"emergencyContact"`
-	CreateDateTime    time.Time     `json:"createDateTime"`
-	UpdateDateTime    time.Time     `json:"updateDatetime"`
-}
-
-type Emergency struct {
-	Name         string `json:"name"`
-	Relationship string `json:"relationship"`
-	Tel          string `json:"tel"`
-}
 
 type Counter interface {
 	count() (int, error)
 }
 
-type MongoCounter struct {}
+type MongoCounter struct{}
 
 func (m MongoCounter) count() (int, error) {
 	db, err := database.Connect()
---
+++ go-api/src/patient/patient.go
@@ -0,0 +1,34 @@
+package patient
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Patient struct {
+	Id                bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	IdPatient         string        `json:"idPatient"`
+	FirstName         string        `json:"firstname"`
+	Lastname          string        `json:"lastname"`
+	Nickname          string        `json:"nickname"`
+	Gender            string        `json:"gender"`
+	Birthday          string        `json:"birthday"`
+	IdCard            string        `json:"idCard"`
+	Career            string        `json:"career"`
+	Tel               string        `json:"tel"`
+	WorkAddress       string        `json:"workAddress"`
+	HomeAddress       string        `json:"homeAddress"`
+	RequiredDocument  string        `json:"requiredDocument"`
+	CongenitalDisease string        `json:"congenitalDisease"`
+	BeAllergic        string        `json:"beAllergic"`
+	EmergencyContact  Emergency     `json:"emergencyContact"`
+	CreateDateTime    time.Time     `json:"createDateTime"`
+	UpdateDateTime    time.Time     `json:"updateDatetime"`
+}
+
+type Emergency struct {
+	Name         string `json:"name"`
+	Relationship string `json:"relationship"`
+	Tel          string `json:"tel"`
+}
