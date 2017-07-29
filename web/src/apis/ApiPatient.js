35220be6c7db66ca47586723ce4eedb7f243878c
phatcharaphan <phat.pan@gmail.com>
2017-07-29 19:03:34 +0700

    #27 add search in generate medical certificate page

--- web/src/apis/ApiPatient.js
+++ web/src/apis/ApiPatient.js
@@ -10,6 +10,10 @@ export const apiValidateSearch=(data)=>{
   return axios.post(config.SEARCH_PATIENT_URL,data).then(responseFromSearch => responseFromSearch.data);
 }
 
+export const apiSearchPatientById=(id)=>{
+  return axios.get(config.SEARCH_PATIENT_URL+ "/" +id).then(responseFromSearch => responseFromSearch.data);
+}
+
 export const apiUpdatePatient=(data)=>{
   return axios.post(config.UPDATE_PATIENT_URL , data).then(response => response.data);
 }
--- web/src/containers/print/GenerateMedicalCertificateContainer.js
+++ web/src/containers/print/GenerateMedicalCertificateContainer.js
@@ -1,8 +1,37 @@
 import React, {Component} from 'react'
+import {Form} from 'react-bootstrap';
+import SearchByIdComponent from '../../components/SearchByIdComponent'
+import DisplayPatientNameComponent from '../../components/DisplayPatientNameComponent'
+import { apiSearchPatientById } from '../../apis/ApiPatient';
 export default class GenerateMedicalCertificateContainer extends Component{
+    state = {
+        namePatient: ""
+    }
+    onChangeIdPatient=(event)=>{
+        this.setState({
+            namePatient: ""
+        })
+        if(event.target.value !== ''){
+            apiSearchPatientById(event.target.value).then(this.searchPatientSuccess)
+        }
+    }
+    searchPatientSuccess=(response)=>{
+        this.setState({
+            namePatient: "Data not found."
+        })
+        if(response){
+            this.setState({
+                 namePatient: response.firstname + " " + response.lastname,
+            })
+        }
+    }
     render(){
         return(
-            <div>GenerateMedicalCertificateContainer</div>
+            <Form horizontal>
+                <SearchByIdComponent onAdvanceSearchPatient={this.onAdvanceSearchPatient}
+                                    onChangeIdPatient={this.onChangeIdPatient}/>
+                <DisplayPatientNameComponent namePatient={this.state.namePatient}/>
+            </Form>
         )
     }
 }
