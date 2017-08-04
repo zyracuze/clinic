afa28f18918cfc811835c548e763f30c9af3dde7
phatcharaphan <phat.pan@gmail.com>
2017-08-04 13:49:51 +0700

    #28, #25 add button back and logout

--- web/src/components/HeaderComponent.js
+++ web/src/components/HeaderComponent.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react'
 import '../App.css';
+import {Grid, Row, Col, Button} from 'react-bootstrap';
 export default class HeaderComponent extends Component {
   render() {
     return(
-        <img src="clinic.png" className="App-logo"  alt={"clinic"}/>
+      <div>
+        <Grid>
+          <Row>
+              <Col xs={4} md={4} className="App-home" >
+               <a href="#/home">
+                <img src="home.png" className="home-size" alt={"clinic"}/> กลับสู่หน้าหลัก
+               </a>
+              </Col>
+              <Col xs={4} md={4}>
+                <img src="clinic.png" className="App-logo"  alt={"clinic"}/>
+              </Col>
+              <Col xs={4} md={4} className="App-logout">
+                <a href="#/">
+                 <img src="logout.png" className="logout-size" alt={"clinic"}/> ออกจากระบบ
+                </a>
+              </Col>
+          </Row>
+          </Grid>
+        </div>
     )
   }
 }
